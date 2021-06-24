2a66224f5dba0591796e34be9fe8cc0fc0fde815
Eric Schwartz <erics273@gmail.com>
2021-06-24 14:50:09 -0400

    completed basic requirements

---
+++ assets/javascript/main.js
@@ -0,0 +1,37 @@
+let dieRolls = [];
+
+let diceRollButton = document.querySelector("#rollButton");
+let numDice = document.querySelector("#numDice");
+let totalSpan = document.querySelector("#total");
+let showAllRollsBtn = document.querySelector("#showAllBtn");
+let rollsList = document.querySelector("#allRolls");
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+  }
+
+diceRollButton.addEventListener("click", function(event){
+    let howManyToRoll = numDice.value;
+
+    let sum = 0;
+    for(let i=1; i <= howManyToRoll; i++){
+        let currentRoll = getRandomIntInclusive(1,6);
+        dieRolls.push(currentRoll);
+        sum += currentRoll
+    }
+    totalSpan.innerHTML = sum
+
+})
+
+showAllRollsBtn.addEventListener("click", function(event){
+
+//loop that loops over the dieRolls array and adds a <li> to the <Ol> using .innerHTML
+    for(let i=0; i < dieRolls.length; i++){
+
+        rollsList.innerHTML += "<li>" + dieRolls[i] + "</li>"
+
+    }
+    
+})
